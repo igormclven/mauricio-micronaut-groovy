ef9288f6f72c83ac3931a361e5a4ad40bae8f59a
iGor McLven (Mauricio) <igormclven>
2021-11-26 22:33:36 +0000

    Initial commit

---
+++ src/test/groovy/com/mauricio/demo/MauricioMicronautGroovyTest.groovy
@@ -0,0 +1,19 @@
+package com.mauricio.demo
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Test
+import jakarta.inject.Inject
+
+@MicronautTest
+class MauricioMicronautGroovyTest {
+
+    @Inject
+    EmbeddedApplication<?> application
+
+    @Test
+    void testItWorks() {
+        assert application.running == true
+    }
+
+}
